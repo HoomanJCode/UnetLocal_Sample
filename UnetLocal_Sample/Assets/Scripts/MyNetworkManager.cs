42cd16a2bf9f089e64bb47374448b85c95385993
Hooman <hooman.jalalpoor@gmail.com>
2019-07-17 12:14:17 +0430

    MyNetworkManager Script

---
+++ UnetLocal_Sample/Assets/Scripts/MyNetworkManager.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class MyNetworkManager : NetworkManager
+{
+}
