40dbac14c603160f71cf733a5dfe6303620b4c74
Hooman <hooman.jalalpoor@gmail.com>
2019-07-17 15:36:40 +0430

    create camera manager script

---
+++ UnetLocal_Sample/Assets/Scripts/CameraManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    // Update is called once per frame
+    private void Update()
+    {
+        //look at my player
+        // ReSharper disable once Unity.PerformanceCriticalCodeNullComparison
+        if(PlayerManager.MyPlayer!=null)
+            transform.LookAt(PlayerManager.MyPlayer.transform);
+    }
+}
