fe36c7e43da1952c54968a9e42cf3c449e14627d
Hooman <hooman.jalalpoor@gmail.com>
2019-07-17 13:18:36 +0430

    Search game button added
    we can broadcast host ip over network  and serach for hosts over network on client.
    this functionality possible by networkDiscovery class
    now my network discovery class created.
    network tested!

---
+++ UnetLocal_Sample/Assets/Scripts/MyNetworkDiscovery.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+[RequireComponent(typeof(MyNetworkManager))]
+public class MyNetworkDiscovery : NetworkDiscovery
+{
+    //get network manager component
+    private MyNetworkManager Manager => GetComponent<MyNetworkManager>();
+
+    public override void OnReceivedBroadcast(string fromAddress, string data)
+    {
+        //you searching for server
+        //if your device found server that broadcasting ip, you can connect to that server from this override
+        Manager.JoinGame(fromAddress);
+        //after join we should stop this function.
+        //if you remove this line, game scene loading for multiple time :D and connecting for multiple time.
+        //this override call multiple time when searching
+        StopBroadcast();
+        //attention you can send maximum player and some info about your server over "data" parameter. default is "Hello" string.
+    }
+}
--- UnetLocal_Sample/Assets/Scripts/MyNetworkManager.cs
+++ UnetLocal_Sample/Assets/Scripts/MyNetworkManager.cs
@@ -1,19 +1,46 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.UI;
 
+[RequireComponent(typeof(MyNetworkDiscovery))]
 public class MyNetworkManager : NetworkManager
 {
+    //get network discovery component
+    private MyNetworkDiscovery Discovery => GetComponent<MyNetworkDiscovery>();
+
+    private void Start()
+    {
+        //this require on network discovery
+        Discovery.Initialize();
+    }
+
+    public void SearchGame()
+    {
+        //start search for servers over network
+        Discovery.StartAsClient();
+    }
+
     public void HostGame()
     {
+        //broadcast i am server :D
+        Discovery.StartAsServer();
+        //start server over this ip
         StartHost();
     }
 
     public void JoinGame(InputField ip)
     {
+        //set target ip
         networkAddress = ip.text;
+        //connect to server
+        StartClient();
+    }
+    public void JoinGame(string ip)
+    {
+        networkAddress = ip;
         StartClient();
     }
 }
