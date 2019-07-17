541190a537e0cb0317056f6501ca7ad0438a2fb8
Hooman <hooman.jalalpoor@gmail.com>
2019-07-17 12:17:17 +0430

    Update MyNetworkManager.cs
    Now we have 2 function that satisfy our 2 GUI functionality.

--- UnetLocal_Sample/Assets/Scripts/MyNetworkManager.cs
+++ UnetLocal_Sample/Assets/Scripts/MyNetworkManager.cs
@@ -6,4 +6,14 @@ using UnityEngine.UI;
 
 public class MyNetworkManager : NetworkManager
 {
+    public void HostGame()
+    {
+        StartHost();
+    }
+
+    public void JoinGame(InputField ip)
+    {
+        networkAddress = ip.text;
+        StartClient();
+    }
 }
